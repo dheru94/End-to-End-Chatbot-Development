bdd25253d1ed716f6f59619364fea2c06ec1c6f0
Dheeraj Kumar <9411304965d@gmail.com>
2025-05-01 18:41:39 +0530

    database changes

--- db_helper.py
+++ db_helper.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 connection = mysql.connector.connect(
             host = os.environ['HOST_NAME'],
+            port = os.environ['PORT_ADD'],
             user = os.environ['DB_USER'],
             password = os.environ['DB_PASSWORD'],
             database = os.environ['DB_NAME']
